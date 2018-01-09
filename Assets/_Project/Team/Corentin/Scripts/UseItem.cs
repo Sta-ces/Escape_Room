c17b4f5ada3e3b61ad6332099aa38155b96b2583
CorentinGengler <gengcor@gmail.com>
2018-01-09 11:41:52 +0100

    nyu

--- Assets/_Project/Team/Corentin/Scripts/UseItem.cs
+++ Assets/_Project/Team/Corentin/Scripts/UseItem.cs
@@ -4,13 +4,54 @@ using UnityEngine;
 
 public class UseItem : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    #region Public Members
+    public GameObject m_playerCharacter;
+    #endregion
+
+
+    #region Public Void
+    public void ItemInteractionStart()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+            Debug.DrawLine(ray.origin, hit.point);
+    }
+
+    public void ItemInteractionStop()
+    {
+
+    }
+
+    #endregion
+
+
+    #region System
+    /*
+    void Start()
+    {
+    }
+    void Awake()
+    {
+    }
+
+    void Update()
+    {
+    }
+    */
+    #endregion
+
+    #region Private Void
+
+    #endregion
+
+    #region Tools Debug And Utility
+
+    #endregion
+
+
+    #region Private And Protected Members
+
+    #endregion
 }
