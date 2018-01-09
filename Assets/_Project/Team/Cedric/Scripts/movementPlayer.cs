2ce3b669a520626e5c01b9e14f1a827fca24f183
Staces <staescedric@gmail.com>
2018-01-09 15:48:27 +0100

    move camera

--- Assets/_Project/Team/Cedric/Scripts/movementPlayer.cs
+++ Assets/_Project/Team/Cedric/Scripts/movementPlayer.cs
@@ -14,11 +14,17 @@ public class movementPlayer : MonoBehaviour
 
     public e_Player m_playerName = e_Player.Player1; // The Rewired player id of this character
 
+    public Camera m_cameraPlayer;
+
     public float m_moveSpeed = 3.0f;
+    public float m_cameraSpeed = 100.0f;
+    /*public float m_minRotationVertical = 15f;
+    public float m_maxRotationVertical = -55f;*/
 
     private Player m_player; // The Rewired Player
     private Rigidbody m_rigidbody;
     private Vector3 m_moveVector;
+    private Vector3 m_cameraVector;
 
     void Awake()
     {
@@ -53,16 +59,21 @@ public class movementPlayer : MonoBehaviour
 
         m_moveVector.x = m_player.GetAxis("MoveHorizontal"); // get input by name or action id
         m_moveVector.z = m_player.GetAxis("MoveVertical");
+        m_cameraVector.y = m_player.GetAxis("CameraHorizontal");
+        m_cameraVector.x = m_player.GetAxis("CameraVertical");
+
+        if (m_player.GetButtonDown("Use"))
+        {
+            Debug.Log("Use");
+        }
     }
 
     private void ProcessInput()
     {
         // Process movement
         m_rigidbody.transform.Translate(m_moveVector * m_moveSpeed * Time.deltaTime);
+        m_rigidbody.transform.Rotate(0, m_cameraVector.y * m_cameraSpeed * Time.deltaTime, 0, Space.World);
 
-        if (m_player.GetButtonDown("Use"))
-        {
-            Debug.Log("Use");
-        }
+        m_cameraPlayer.transform.Rotate(m_cameraVector.x * m_cameraSpeed * Time.deltaTime, 0, 0, Space.Self);
     }
 }
