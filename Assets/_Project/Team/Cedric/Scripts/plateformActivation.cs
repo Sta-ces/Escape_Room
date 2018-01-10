d355050f3c48cd4623330cf8dbbe96b350661205
Staces <staescedric@gmail.com>
2018-01-10 16:34:59 +0100

    Commit apres mise en commun pour l egnime

---
+++ Assets/_Project/Team/Cedric/Scripts/plateformActivation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plateformActivation : MonoBehaviour {
+
+    public GameObject m_objectToPutt;
+    public bool m_actived = false;
+
+    private void OnCollisionStay(Collision other) {
+        if(other.gameObject == m_objectToPutt)
+        {
+            Debug.Log(gameObject.name+" actived");
+            m_actived = true;
+        }
+    }
+}
