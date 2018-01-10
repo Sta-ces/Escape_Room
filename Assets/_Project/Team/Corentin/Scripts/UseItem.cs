288e217d356191167c835af2798ec5db6f1bf7a8
CorentinGengler <gengcor@gmail.com>
2018-01-10 16:35:09 +0100

    Unfreeze Dropped items

--- Assets/_Project/Team/Corentin/Scripts/UseItem.cs
+++ Assets/_Project/Team/Corentin/Scripts/UseItem.cs
@@ -57,9 +57,9 @@ public class UseItem : MonoBehaviour {
             GameObject objToDrop = m_myInventory.GetCurrentlyEquippedItem().obj;
             objToDrop.transform.parent = null;
             m_myInventory.RemoveCurrentItem();
+            objToDrop.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
             StartCoroutine("TimerAfterDropEquippedItem");
             m_canDropEquipped = false;
-            Debug.Log("yop");
         }
     }
     //--------------------------------------------------------
@@ -147,7 +147,6 @@ public class UseItem : MonoBehaviour {
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit , 100f))
         {
-            //Debug.Log(hit.transform.gameObject.name);
             if (hit.distance < m_maxDistanceInteraction)//if the object is close enough to be interacted with
             {
                 if (hit.transform.gameObject.tag == "Movable")
